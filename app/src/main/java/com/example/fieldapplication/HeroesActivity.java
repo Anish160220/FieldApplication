039f5efb4f2e88cfa2fb877c0fcd04176d9d02ce
[anish rajbhandari] <[anishkalpakunj@gmail.com]>
2019-05-21 21:16:30 +0545

    Initial commit

---
+++ app/src/main/java/com/example/fieldapplication/HeroesActivity.java
@@ -0,0 +1,55 @@
+package com.example.fieldapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.HeroesAdapter;
+import heroesapi.HeroesAPI;
+import model.Heroes;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import url.Url;
+
+public class HeroesActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    List<Heroes> heroesList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heroes);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        HeroesAPI heroesAPI = Url.getInstance().create(HeroesAPI.class);
+
+         Call<List<Heroes>> listCall = heroesAPI.getAllHeroes();
+        listCall.enqueue(new Callback<List<Heroes>>() {
+            @Override
+            public void onResponse(Call<List<Heroes>> call, Response<List<Heroes>> response) {
+                heroesList = response.body();
+                HeroesAdapter heroesAdapter = new HeroesAdapter(HeroesActivity.this,heroesList);
+                recyclerView.setAdapter(heroesAdapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(HeroesActivity.this));
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Heroes>> call, Throwable t) {
+                Toast.makeText(HeroesActivity.this, "Error : "+t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+
+            }
+
+
+        });
+
+            }
+}
---
+++ app/src/main/java/model/Heroes.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Heroes {
+    private String _id,name,desc,image;
+
+    public Heroes(String name, String desc, String image) {
+        this.name = name;
+        this.desc = desc;
+        this.image = image;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
